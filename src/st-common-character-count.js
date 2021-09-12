1940f25b268bee776517933166e104f0ea1c5166
SavelyevNikita <nik2111122@sibmail.com>
2021-09-12 14:45:21 +0300

    feat: st-common-character-count

--- src/st-common-character-count.js
+++ src/st-common-character-count.js
@@ -11,7 +11,20 @@ import { NotImplementedError } from '../extensions/index.js';
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-export default function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function getCommonCharacterCount(s1, s2) {
+    // throw new NotImplementedError('Not implemented');
+    // remove line with error and write your code here
+    let count = 0;
+    const temp = [];
+    loopi:
+        for (let i = 0; i < s1.length; i += 1) {
+            for (let j = 0; j < s2.length; j += 1) {
+                if (s1[i] === s2[j] && !temp.includes(j)) {
+                    temp.push(j);
+                    count += 1;
+                    continue loopi;
+                }
+            }
+        }
+    return count;
 }
--- src/what-season.js
+++ src/what-season.js
@@ -11,7 +11,16 @@ import { NotImplementedError } from '../extensions/index.js';
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-export default function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function getSeason(date) {
+    // throw new NotImplementedError('Not implemented');
+    // remove line with error and write your code here
+    const typeDate = typeof date;
+    if (date === undefined) return 'Unable to determine the time of year!';
+    console.log(typeof date.getUTCMonth === 'function');
+    if (typeof date.getMonth === 'function') {
+        if (date.getMonth() >= 2 && date.getMonth() <= 4) return 'spring';
+        if (date.getMonth() >= 5 && date.getMonth() <= 7) return 'summer';
+        if (date.getMonth() >= 8 && date.getMonth() <= 10) return 'autumn';
+        return 'winter';
+    } else return 'Invalid date!';
 }

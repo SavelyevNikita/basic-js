c1f01b5aba9e4a2bacd89099291990dc7fabd261
SavelyevNikita <nik2111122@sibmail.com>
2021-09-13 23:18:44 +0300

    feat: what-season

--- src/what-season.js
+++ src/what-season.js
@@ -12,15 +12,10 @@ import { NotImplementedError } from '../extensions/index.js';
  * 
  */
 export default function getSeason(date) {
-    // throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-    const typeDate = typeof date;
-    if (date === undefined) return 'Unable to determine the time of year!';
-    console.log(typeof date.getUTCMonth === 'function');
-    if (typeof date.getMonth === 'function') {
-        if (date.getMonth() >= 2 && date.getMonth() <= 4) return 'spring';
-        if (date.getMonth() >= 5 && date.getMonth() <= 7) return 'summer';
-        if (date.getMonth() >= 8 && date.getMonth() <= 10) return 'autumn';
-        return 'winter';
-    } else return 'Invalid date!';
+    if (date === undefined) return ('Unable to determine the time of year!');
+    if ((Object.keys(date).length !== 0) || (typeof date.getMonth !== 'function')) throw Error('Invalid date!');
+    if (date.getMonth() >= 2 && date.getMonth() <= 4) return 'spring';
+    if (date.getMonth() >= 5 && date.getMonth() <= 7) return 'summer';
+    if (date.getMonth() >= 8 && date.getMonth() <= 10) return 'autumn';
+    return 'winter';
 }

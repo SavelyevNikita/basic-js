883a3bd52322cc0a9b468bb4853c76b984c29e26
SavelyevNikita <nik2111122@sibmail.com>
2021-09-17 18:09:57 +0300

    feat: exercise

--- src/st-encode-line.js
+++ src/st-encode-line.js
@@ -10,7 +10,20 @@ import { NotImplementedError } from '../extensions/index.js';
  * For aabbbc should return 2a3bc
  *
  */
-export default function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function encodeLine(str) {
+    // throw new NotImplementedError('Not implemented');
+    // remove line with error and write your code here
+    const cleanString = [];
+    let string = '';
+    for (const item of str) {
+        if (!cleanString.includes(item)) cleanString.push(item);
+    }
+    for (const itemCleanString of cleanString) {
+        let count = 0;
+        for (const item of str) {
+            if (itemCleanString === item) count += 1;
+        }
+        string += count + itemCleanString;
+    }
+    return string;
 }

52699b951523eefead21959308d62f2958d81111
SavelyevNikita <nik2111122@sibmail.com>
2021-09-20 22:27:32 +0300

    feat: transform-array.js

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,7 +13,39 @@ import { NotImplementedError } from '../extensions/index.js';
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-export default function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function transform(arr) {
+    // throw new NotImplementedError('Not implemented');
+    // remove line with error and write your code here
+    if (!Array.isArray(arr)) throw Error("\'arr\' parameter must be an instance of the Array!");
+    const cloneArray = arr.map(item => item);
+    const newReturnableArray = [];
+    // console.log(newArray);
+    let cloneArrayLength = cloneArray.length;
+    for (let i = 0; i < cloneArrayLength; i += 1) {
+        if (cloneArray[i] === '--double-next' && i === cloneArrayLength - 1) continue;
+        if (cloneArray[i] === '--discard-next' && i === cloneArrayLength - 1) continue;
+        if (cloneArray[i] === '--discard-prev' && i === 0) continue;
+        if (cloneArray[i] === '--double-prev' && i === 0) continue;
+        if (cloneArray[i] === '--double-next') {
+            newReturnableArray.push(cloneArray[i + 1]);
+            continue;
+        };
+        if (cloneArray[i] === '--double-prev') {
+            newReturnableArray.push(cloneArray[i - 1]);
+            continue;
+        };
+        if (cloneArray[i] === '--discard-prev') {
+            newReturnableArray.pop();
+            continue;
+        };
+        if (cloneArray[i] === '--discard-next') {
+            // newReturnableArray.push('');
+            i += 2;
+            continue;
+        };
+
+        newReturnableArray.push(cloneArray[i]);
+    }
+
+    return newReturnableArray;
 }

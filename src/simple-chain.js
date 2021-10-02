8c832f8dbc8ae364da803eb063bbbee53c932caf
SavelyevNikita <nik2111122@sibmail.com>
2021-10-02 13:34:43 +0300

    feat:simple-chain.js.js

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -2,27 +2,50 @@ import { NotImplementedError } from '../extensions/index.js';
 
 /**
  * Implement chainMaker object according to task description
- * 
+ *
  */
 export default {
-  getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    arr: [],
+    getLength() {
+        // throw new NotImplementedError('Not implemented');
+        // remove line with error and write your code here
+        return this.arr.length
+    },
+    addLink(value) {
+        // throw new NotImplementedError('Not implemented');
+        // remove line with error and write your code here
+        this.arr.push(value);
+        return this;
+    },
+    removeLink(position) {
+        // throw new NotImplementedError('Not implemented');
+        // remove line with error and write your code here
+        if (position <= 0 || typeof position !== 'number' || position > this.arr.length) {
+            this.arr = [];
+            throw new Error(`You can't remove incorrect link!`);
+        }
+        this.arr.splice(position - 1, 1);
+        return this;
+    },
+    reverseChain() {
+        // throw new NotImplementedError('Not implemented');
+        // remove line with error and write your code here
+        this.arr.reverse();
+        return this;
+    },
+    finishChain() {
+        // throw new NotImplementedError('Not implemented');
+        // remove line with error and write your code here
+        let result = '';
+        for (let i = 0; i < this.arr.length; i++) {
+            if (i === 0) {
+                result += '( ' + this.arr[i] + ' )';
+                continue;
+            }
+
+            result += '~~' + '( ' + this.arr[i] + ' )';
+        }
+        this.arr = [];
+        return result;
+    }
 };
